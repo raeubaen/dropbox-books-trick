3fc17c474c1d6a88b534e42ff930c3310bc5177d
Ruben Gargiulo <ruben@membri.com>
2026-05-10 11:07:50 +0200

    name added

--- blessed_new3.py
+++ blessed_new3.py
@@ -1,3 +1,5 @@
+import glob
+import subprocess
 import os
 import time
 import urllib.parse
@@ -15,161 +17,172 @@ from functions import *
 
 import argparse
 
-print("init")
+def run(name, url, out_dir, firefox_bin, geckodriver):
 
-parser = argparse.ArgumentParser()
+  os.makedirs(f"{out_dir}", exist_ok=True)
+  os.makedirs(f"{out_dir}/\"Pagine da {name}\"", exist_ok=True)
 
-parser.add_argument("--url", required=True)
-parser.add_argument("--out_dir", default="pages_html")
-parser.add_argument("--firefox_bin", default="/usr/bin/firefox")
-parser.add_argument("--geckodriver", default="/home/ruben/Downloads/geckodriver")
+  options = Options()
+  options.add_argument("--headless")
+  options.headless = True
+  options.binary_location = args.firefox_bin
 
-args = parser.parse_args()
+  service = Service(executable_path=args.geckodriver)
 
-print("parsing")
+  print("accendendo firefox")
+  driver = webdriver.Firefox(service=service, options=options)
 
-URL = args.url
+  print("caricando la pagina")
+  driver.get(url)
+  print("sleep 5")
+  time.sleep(5)
 
-OUT_DIR = args.out_dir
-os.makedirs(OUT_DIR, exist_ok=True)
+  print("scrolling")
+  driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
+  print("sleep 3")
+  time.sleep(3)
 
+  seen_pages = {}
+  last_new_pages = 0
+  stuck_counter = 0
 
-options = Options()
-options.add_argument("--headless")
-options.headless = True
-options.binary_location = args.firefox_bin
+  print("entrando nel loop di scroll")
+  while True:
+      html = driver.page_source
+      soup = BeautifulSoup(html, "html.parser")
 
-service = Service(executable_path=args.geckodriver)
+      current_pages = soup.find_all(
+          "div",
+          id=lambda x: x and "pageContainer" in x
+      )
 
-print("accendendo firefox")
-driver = webdriver.Firefox(service=service, options=options)
+      new_count = 0
 
-print("caricando la pagina")
-driver.get(URL)
-print("sleep 5")
-time.sleep(5)
+      for p in current_pages:
+          pid = p.get("id")
 
-print("scrolling")
-driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
-print("sleep 3")
-time.sleep(3)
+          if pid not in seen_pages:
 
-seen_pages = {}
-last_new_pages = 0
-stuck_counter = 0
+              print("aspettando immagine, pagina: ", pid)
+              try:
+                src = WebDriverWait(driver, 30).until(
+                    lambda d: d.execute_script(f"""
+                        const el = document.getElementById("{pid}");
+                        if (!el) return null;
 
-print("entrando nel loop di scroll")
-while True:
+                        const img = el.querySelector("img");
+                        if (!img) return null;
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+                        const s = img.getAttribute("src");
 
-    current_pages = soup.find_all(
-        "div",
-        id=lambda x: x and "pageContainer" in x
-    )
+                        if (!s || s.length < 10 || s.startsWith("data:")) return null;
 
-    new_count = 0
+                        return s;
+                    """)
+                )
+              except TimeoutException:
+                break
 
-    for p in current_pages:
-        pid = p.get("id")
+              time.sleep(0.3)
 
-        if pid not in seen_pages:
+              print("disponibile immagine, src:", src)
 
-            print("aspettando immagine, pagina: ", pid)
-            try:
-              src = WebDriverWait(driver, 30).until(
-                  lambda d: d.execute_script(f"""
-                      const el = document.getElementById("{pid}");
-                      if (!el) return null;
+              fresh_el = driver.find_element("id", pid)
+              html = driver.execute_script("return arguments[0].outerHTML;", fresh_el)
+              p_bs = BeautifulSoup(html, "html.parser")
 
-                      const img = el.querySelector("img");
-                      if (!img) return null;
+              seen_pages[pid] = p_bs
 
-                      const s = img.getAttribute("src");
+              new_count += 1
+              print("preso:", pid)
 
-                      if (!s || s.length < 10 || s.startsWith("data:")) return null;
+      if new_count == 0:
+          stuck_counter += 1
+      else:
+          stuck_counter = 0
 
-                      return s;
-                  """)
-              )
-            except TimeoutException:
-              break
+      print("scrolling")
 
-            time.sleep(0.3)
-
-            print("disponibile immagine, src:", src)
-
-            fresh_el = driver.find_element("id", pid)
-            html = driver.execute_script("return arguments[0].outerHTML;", fresh_el)
-            p_bs = BeautifulSoup(html, "html.parser")
-
-            seen_pages[pid] = p_bs
-
-            new_count += 1
-            print("preso:", pid)
-
-    if new_count == 0:
-        stuck_counter += 1
-    else:
-        stuck_counter = 0
-
-    print("scrolling")
-
-    driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
+      driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
 
 
-    if stuck_counter > 5:
-        print("breaking")
-        break
+      if stuck_counter > 5:
+          print("breaking")
+          break
 
-driver.quit()
-
-
-pages = list(seen_pages.values())
-
-pages = sorted(pages, key=page_id)
-
-driver = webdriver.Firefox(
-    service=service,
-    options=options
-)
+  driver.quit()
 
 
-#si poteva fare prima ma il driver si incazza ...
-for i, page in enumerate(pages):
+  pages = list(seen_pages.values())
 
-    page_html = str(page)
+  pages = sorted(pages, key=page_id)
 
-    svg_class = extract_single_svg_class(page)
-    text_classes = extract_svg_text_classes(page)
-    svg_style = build_svg_style(svg_class, text_classes)
+  driver = webdriver.Firefox(
+      service=service,
+      options=options
+  )
 
-    final_html = wrap_page(page_html, svg_style)
 
-    out_path = os.path.join(OUT_DIR, f"page_{i}.html")
+  #si poteva fare prima ma il driver si incazza ...
+  for i, page in enumerate(pages):
 
-    abs_path = os.path.abspath(out_path)
+      page_html = str(page)
 
-    with open(abs_path, "w", encoding="utf-8") as f:
-        f.write(final_html)
+      svg_class = extract_single_svg_class(page)
+      text_classes = extract_svg_text_classes(page)
+      svg_style = build_svg_style(svg_class, text_classes)
 
-    driver.get(f"file://{abs_path}")
+      final_html = wrap_page(page_html, svg_style)
 
-    print_options = PrintOptions()
+      out_path = os.path.join(f"{out_dir}/\"Pagine da {name}\"", f"page_{i}.html")
 
-    print_options.background = True
-    print_options.orientation = "portrait" #non sono sicuro
-    print_options.scale = 1
+      abs_path = os.path.abspath(out_path)
 
-    pdf_data = driver.print_page(print_options)
+      with open(abs_path, "w", encoding="utf-8") as f:
+          f.write(final_html)
 
-    pdf_path = os.path.join(OUT_DIR, f"page_{i}.pdf")
+      driver.get(f"file://{abs_path}")
 
-    with open(pdf_path, "wb") as f:
-        f.write(base64.b64decode(pdf_data))
+      print_options = PrintOptions()
 
-    print("salvato pdf:", pdf_path)
+      print_options.background = True
+      print_options.orientation = "portrait" #non sono sicuro
+      print_options.scale = 1
 
-driver.quit()
+      pdf_data = driver.print_page(print_options)
+
+      pdf_path = os.path.join(f"{out_dir}/\"Pagine da {name}\"", f"page_{i}.pdf")
+
+      with open(pdf_path, "wb") as f:
+          f.write(base64.b64decode(pdf_data))
+
+      print("salvato pdf:", pdf_path)
+
+
+  files = sorted(glob.glob(f"{out_dir}/\"Pagine da {name}\"/page_*.pdf"))
+
+  output = f"{out_dir}/{name.replace('.pdf','')}.pdf"
+
+  subprocess.run(
+      ["pdfunite", *files, output],
+      check=True
+  )
+  driver.quit()
+
+
+
+if __name__ == "__main__":
+  print("init")
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("--name", required=True)
+  parser.add_argument("--url", required=True)
+  parser.add_argument("--out_dir", default="pages_html")
+  parser.add_argument("--firefox_bin", default="/usr/bin/firefox")
+  parser.add_argument("--geckodriver", default="/home/ruben/Downloads/geckodriver")
+
+  args = parser.parse_args()
+
+  run(args.name, args.url, args.out_dir, args.firefox_bin, args.geckodriver)
 
